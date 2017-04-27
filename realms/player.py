fd55d717985d0c53a26a35ebfe3b10768814013f
Zach Mitchell <zmitchell@fastmail.com>
2017-04-27 14:00:17 -0400

    Added a player class

---
+++ realms/player.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+.. module:: player
+    :synopsis: Provides the Player class
+.. moduleauthor:: Zach Mitchell <zmitchell@fastmail.com>
+"""
+
+
+from deck import PlayerDeck
+
+
+class Player(object):
+    """
+    Represents a single player
+    """
+    def __init__(self, name):
+        self.name = name
+        self._deck = PlayerDeck()
+        self._cards_drawn = self._deck.draw()  # draws 5 by default
+        self._bases = None
+        self._queued_effects = None
+        self._health = 50
+        self._money = 0
+        pass
+
+
