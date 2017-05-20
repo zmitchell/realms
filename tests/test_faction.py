2124550cf32ad391aaee31a86f989c6e91537ba5
Zach Mitchell <zmitchell@fastmail.com>
2017-05-20 11:31:03 -0400

    Updated test to reflect name change

--- tests/test_faction.py
+++ tests/test_faction.py
@@ -1,6 +1,8 @@
 from realms.card import Faction
+from realms.card import CardFaction
 
 
 def test_faction_str_representation():
     fac = Faction.star
+    fac = CardFaction.star
     assert str(fac) == 'Star Empire'
