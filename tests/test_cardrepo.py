8f0c5ea54e3e5f8b873e590726bc4cf6f8f3b000
Zach Mitchell <zmitchell@fastmail.com>
2017-05-23 12:18:15 -0400

    Renamed tests

--- tests/test_cardrepo.py
+++ tests/test_cardrepo.py
@@ -16,20 +16,20 @@ def viper(repo):
     return repo.new_viper()
 
 
-def test_cardrepo_viper_properties(viper):
+def test_viper_properties(viper):
     assert viper.faction == CardFaction.UNALIGNED
     assert viper.cost == 0
     assert viper.base is False
 
 
-def test_cardrepo_viper_effects_basic(viper):
+def test_viper_effects_basic(viper):
     v_effect = viper.effects_basic[0]
     assert v_effect.target == CardTarget.OPPONENT
     assert v_effect.action == CardAction.ATTACK
     assert v_effect.value == 1
 
 
-def test_cardrepo_viper_effects_number(viper):
+def test_viper_effects_number(viper):
     assert len(viper.effects_basic) == 1
     assert len(viper.effects_ally) == 0
     assert len(viper.effects_scrap) == 0
@@ -41,20 +41,20 @@ def scout(repo):
     return repo.new_scout()
 
 
-def test_cardrepo_scout_properties(scout):
+def test_scout_properties(scout):
     assert scout.faction == CardFaction.UNALIGNED
     assert scout.cost == 0
     assert scout.base is False
 
 
-def test_cardrepo_scout_effects_basic(scout):
+def test_scout_effects_basic(scout):
     s_effect = scout.effects_basic[0]
     assert s_effect.target == CardTarget.OWNER
     assert s_effect.action == CardAction.MONEY
     assert s_effect.value == 1
 
 
-def test_cardrepo_scout_effects_number(scout):
+def test_scout_effects_number(scout):
     assert len(scout.effects_basic) == 1
     assert len(scout.effects_ally) == 0
     assert len(scout.effects_scrap) == 0
@@ -66,27 +66,27 @@ def explorer(repo):
     return repo.new_explorer()
 
 
-def test_cardrepo_explorer_properties(explorer):
+def test_explorer_properties(explorer):
     assert explorer.faction == CardFaction.UNALIGNED
     assert explorer.cost == 0
     assert explorer.base is False
 
 
-def test_cardrepo_explorer_effects_basic(explorer):
+def test_explorer_effects_basic(explorer):
     effect = explorer.effects_basic[0]
     assert effect.target == CardTarget.OWNER
     assert effect.action == CardAction.MONEY
     assert effect.value == 2
 
 
-def test_cardrepo_explorer_effects_scrap(explorer):
+def test_explorer_effects_scrap(explorer):
     effect = explorer.effects_scrap[0]
     assert effect.target == CardTarget.OPPONENT
     assert effect.action == CardAction.ATTACK
     assert effect.value == 2
 
 
-def test_cardrepo_explorer_effects_number(explorer):
+def test_explorer_effects_number(explorer):
     assert len(explorer.effects_basic) == 1
     assert len(explorer.effects_ally) == 0
     assert len(explorer.effects_scrap) == 1
