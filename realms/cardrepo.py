faa812cb3995ea19778989df5c17b7fad9f5fc31
Zach Mitchell <zmitchell@fastmail.com>
2017-05-23 11:38:11 -0400

    Brought pony.orm classes and functions into the module namespace

--- realms/cardrepo.py
+++ realms/cardrepo.py
@@ -5,13 +5,21 @@
 .. moduleauthor:: Zach Mitchell <zmitchell@fastmail.com>
 """
 
-import pony.orm as pny
+from pony.orm import (
+        Database,
+        Required,
+        Set,
+        PrimaryKey,
+        select,
+        commit,
+        db_session
+    )
 from .card import CardFaction, CardAction, CardTarget, Card
 import json
 from pkg_resources import resource_string, resource_exists
 from uuid import uuid4
 
-db = pny.Database()
+db = Database()
 
 
 class CardRepo(object):
@@ -27,19 +35,20 @@ class CardRepo(object):
             db.bind('sqlite', 'realms-cards.sqlite')
             db.generate_mapping()
 
-    @pny.db_session
+    @db_session
     def new_viper(self):
         viper = self._named_card('Viper')
         return viper
 
-    @pny.db_session
+    @db_session
     def new_scout(self):
         scout = self._named_card('Scout')
         return scout
 
-    @pny.db_session
+    @db_session
+    @db_session
     def _named_card(self, cardname):
-        primitive = pny.select(c for c in CardPrimitive if c.name == cardname).first()
+        primitive = select(c for c in CardPrimitive if c.name == cardname).first()
         new_uuid = uuid4()
         card = Card(primitive, new_uuid)
         return card
@@ -48,109 +57,109 @@ class CardRepo(object):
 class FactionPrimitive(db.Entity):
     """The ORM entity representing a ``CardFaction`` member
     """
-    id = pny.PrimaryKey(int, auto=True)
+    id = PrimaryKey(int, auto=True)
     """(int) Numeric identifier for the faction"""
 
-    name = pny.Required(str, unique=True)
+    name = Required(str, unique=True)
     """(str) The display-style string for the faction"""
 
-    cards = pny.Set('CardPrimitive')
+    cards = Set('CardPrimitive')
     """The set of ``CardPrimitive`` entities belonging to this faction"""
 
 
 class TargetPrimitive(db.Entity):
     """The ORM entity representing a ``CardTarget`` member
     """
-    id = pny.PrimaryKey(int, auto=True)
+    id = PrimaryKey(int, auto=True)
     """(int) Numeric identifier for the target"""
 
-    name = pny.Required(str, unique=True)
+    name = Required(str, unique=True)
     """(str) The display-style string for the target"""
 
-    effects = pny.Set('EffectPrimitive')
+    effects = Set('EffectPrimitive')
     """The set of ``EffectPrimitive`` entities with this target"""
 
 
 class ActionPrimitive(db.Entity):
     """The ORM entity representing a ``CardAction`` member
     """
-    id = pny.PrimaryKey(int, auto=True)
+    id = PrimaryKey(int, auto=True)
     """(int) Numeric identifier for the action"""
 
-    name = pny.Required(str)
+    name = Required(str)
     """(str) The display-style string for the action"""
 
-    effects = pny.Set('EffectPrimitive')
+    effects = Set('EffectPrimitive')
     """The set of ``EffectPrimitive`` entities with this action"""
 
 
 class EffectPrimitive(db.Entity):
     """The ORM entity representing a ``CardEffect``
     """
-    id = pny.PrimaryKey(int, auto=True)
+    id = PrimaryKey(int, auto=True)
     """(int) Numeric identifier for the effect (not used)"""
 
-    target = pny.Required(TargetPrimitive)
+    target = Required(TargetPrimitive)
     """The target of the effect"""
 
-    action = pny.Required(ActionPrimitive)
+    action = Required(ActionPrimitive)
     """The action to apply to the target"""
 
-    value = pny.Required(int, size=8)
+    value = Required(int, size=8)
     """The value of the action (how many cards to draw, money provided, etc.)"""
 
-    cards = pny.Set('CardPrimitive', reverse='effects')
+    cards = Set('CardPrimitive', reverse='effects')
     """The set of cards providing this effect (not used)"""
 
-    allies = pny.Set('CardPrimitive', reverse='ally')
+    allies = Set('CardPrimitive', reverse='ally')
     """The set of cards that provide this effect as an ally ability (not used)"""
 
-    scraps = pny.Set('CardPrimitive', reverse='scrap')
+    scraps = Set('CardPrimitive', reverse='scrap')
     """The set of cards that provide this effect when scrapped (not used)"""
 
 
 class CardPrimitive(db.Entity):
     """The ORM entity representing a ``Card``
     """
-    id = pny.PrimaryKey(int, auto=True)
+    id = PrimaryKey(int, auto=True)
     """(int) Numeric identifier of the card"""
 
-    name = pny.Required(str)
+    name = Required(str)
     """(str) The name of the card"""
 
-    faction = pny.Required(FactionPrimitive)
+    faction = Required(FactionPrimitive)
     """(FactionPrimitive) The card's faction"""
 
-    simplified = pny.Required(bool)
+    simplified = Required(bool)
     """(bool) Denotes whether the card's effects have been simplified to ease implementation"""
 
-    base = pny.Required(bool)
+    base = Required(bool)
     """(bool) Denotes whether the card is a base"""
 
-    outpost = pny.Required(bool)
+    outpost = Required(bool)
     """(bool) If the card is a base, denotes whether it is also an outpost"""
 
-    defense = pny.Required(int, size=8)
+    defense = Required(int, size=8)
     """(int) If the card is a base, denotes the damage required to destroy it"""
 
-    cost = pny.Required(int, size=8)
+    cost = Required(int, size=8)
     """(int) The amount of trade required to acquire the card"""
 
-    effects = pny.Set(EffectPrimitive, reverse='cards')
+    effects = Set(EffectPrimitive, reverse='cards')
     """A list of effects provided by the card"""
 
-    ally = pny.Set(EffectPrimitive, reverse='allies')
+    ally = Set(EffectPrimitive, reverse='allies')
     """A list of effects activated when another card of the same faction is played"""
 
-    scrap = pny.Set(EffectPrimitive, reverse='scraps')
+    scrap = Set(EffectPrimitive, reverse='scraps')
     """A list of effects activated when the card is scrapped"""
 
-    count = pny.Required(int, size=8)
+    count = Required(int, size=8)
     """(int) The number of copies of this card present in the main deck
     (0 if unlimited or provided in each player's starting deck)"""
 
 
-@pny.db_session
+@db_session
 def _populate_factions():
     """Populates the entries in the FactionPrimitives table
 
@@ -160,11 +169,11 @@ def _populate_factions():
         The list of FactionPrimitive entities created
     """
     factions = [FactionPrimitive(name=f.value) for f in CardFaction]
-    pny.commit()
+    commit()
     return factions
 
 
-@pny.db_session
+@db_session
 def _populate_actions():
     """Populates the entries in the ActionPrimitives table
 
@@ -174,11 +183,11 @@ def _populate_actions():
         The list of ActionPrimitive entities created
     """
     actions = [ActionPrimitive(name=a.name) for a in CardAction]
-    pny.commit()
+    commit()
     return actions
 
 
-@pny.db_session
+@db_session
 def _populate_targets():
     """Populates the entries in the TargetPrimitives table
 
@@ -188,7 +197,7 @@ def _populate_targets():
         The list of TargetPrimitives created
     """
     targets = [TargetPrimitive(name=t.name) for t in CardTarget]
-    pny.commit()
+    commit()
     return targets
 
 
@@ -199,7 +208,7 @@ def _populate_enums():
     return action_entities, faction_entities, target_entities
 
 
-@pny.db_session
+@db_session
 def _populate_db():
     """Populates the tables in ``realms-cards.sqlite`` from the data in ``cards.json``
     """
@@ -208,11 +217,11 @@ def _populate_db():
     actions, factions, targets = _populate_enums()
     for j in json_cards:
         _populate_card(j, actions, factions, targets)
-        pny.commit()
+        commit()
     return
 
 
-@pny.db_session
+@db_session
 def _populate_card(card, actions, factions, targets):
     """Creates a single CardPrimitive entity from a JSON object
 
@@ -239,7 +248,7 @@ def _populate_card(card, actions, factions, targets):
     return populated_card
 
 
-@pny.db_session
+@db_session
 def _populate_effects(effect_list, targets, actions):
     """Creates a list of EffectsPrimitive entities from a list of JSON objects
 
